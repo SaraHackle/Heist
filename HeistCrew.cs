02cabe38297fb41f736ce5b37da891239fc7f900
Sara Hackle <Shackle@ivytech.edu>
2022-03-04 14:32:51 -0600

    Phase 3

--- HeistCrew.cs
+++ HeistCrew.cs
@@ -1,29 +1,37 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
-namespace Heist 
+namespace Heist
 {
     public class HeistCrew
     {
-        public string Name {get;}
+        public string Name { get; }
         public List<TeamMember> HeistTeam = new List<TeamMember>();
 
         public HeistCrew(string name)
         {
             Name = name;
         }
-
+        //method to add a team member to the HeistTeam
         public void addTeamMember(TeamMember teamMember)
         {
             HeistTeam.Add(teamMember);
         }
 
+        //method that shows the HeistTeam by individual member
         public void displayHeistCrew()
         {
             foreach (TeamMember person in HeistTeam)
             {
                 Console.WriteLine($"Team Member Stats: \n Name: {person.Name} \n Skill Level: {person.SkillLevel} \n Courage Level: {person.Courage}");
             }
+        }
+
+        //  method that creates new list of skill levels and adds them together
+        public int TotalSkill()
+        {
+            return HeistTeam.ConvertAll<int>(team => team.SkillLevel).Sum();
         }
 
     }
--- TeamMember.cs
+++ TeamMember.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 
 namespace Heist 
@@ -17,6 +19,8 @@ namespace Heist
             SkillLevel = skillLevel;
             Courage = courageLevel;
         }
+
+       
 
     }
 
